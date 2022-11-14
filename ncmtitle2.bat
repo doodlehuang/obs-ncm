b1a225b5932f80ffab50d74c743632ecbb326253
Doodle Huang <myjhao@hotmail.com>
2022-11-14 19:16:53 +0800

    Delete ncmtitle2.bat

--- ncmtitle2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f %%i in ('powershell -c "echo (Get-Process cloudmusic | where MainWindowTitle -CMatch " - ").Id"') do set pid=%%i
-:main
-for /f "tokens=10 delims=," %%i in ('tasklist /v /FI "pid eq %pid%" /FO csv') do powershell -c "out-file -inputobject '%%i' -encoding utf8 titletest.txt"
-goto main
